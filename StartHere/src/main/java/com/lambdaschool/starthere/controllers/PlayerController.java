378bd2ed51518c43fb422195889d5d4f58be69b5
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-25 22:45:53 -0500

    Players endpoint works, added documentation

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/PlayerController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/PlayerController.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.starthere.controllers;
 import com.lambdaschool.starthere.models.Player;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,12 +18,9 @@ public class PlayerController
     private static final Logger logger = LoggerFactory.getLogger(RolesController.class);
     private RestTemplate restTemplate = new RestTemplate();
 
-    // taken from https://openlibrary.org/dev/docs/api/books
-    // returns a list of books - you can include multiple ISBNs in a single request
-    // This API returns a map instead of the standard list
-    //
-    // localhost:2019/otherapis/openlibrary/0982477562
+   
 
+    @ApiOperation(value = "Return a player", response = Player.class, responseContainer = "String")
     @GetMapping(value = "/player/{name}",
             produces = {"application/json"})
     public ResponseEntity<?> listAPlayerByName(HttpServletRequest request,
@@ -39,61 +37,5 @@ public class PlayerController
         System.out.println(player);
         return new ResponseEntity<>(player, HttpStatus.OK);
     }
-//    @Autowired
-//    private PlayerService playerService;
-//
-//
-//
-//    //List of all players
-//    @ApiOperation(value = "return all players with stats", response = Player.class, responseContainer = "List")
-//    @GetMapping(value="/players", produces = {"application/json"})
-//    public ResponseEntity<?> listAllPlayers()
-//    {
-//        List<Player> myPlayer = playerService.findAll();
-//        return new ResponseEntity<>(myPlayer, HttpStatus.OK);
-//    }
-//
-//    //Specific player stats
-//    @ApiOperation(value = "Find specific players stats by name", response = Player.class, responseContainer = "List")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "Player Found", response = Player.class),
-//            @ApiResponse(code = 404, message = "Player not found", response = ErrorDetail.class)
-//    })
-//    @GetMapping(value="/players/{name}", produces ={"application/json"})
-//    public ResponseEntity<?> findPlayerByName(@PathVariable String name)
-//    {
-//        Player myPlayer = playerService.findByPlayerName(name);
-//        return new ResponseEntity<>(myPlayer, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping(value = "/delete/{id}")
-//    public ResponseEntity<?> deletePlayerById(
-//            @PathVariable
-//                    long id)
-//    {
-//        playerService.delete(id);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-////    @ApiOperation(value = "Saves a Player to your saved List", response = void.class)
-////    @ApiResponses(value = {
-////            @ApiResponse(code = 201, message = "Player was saved successfully"),
-////            @ApiResponse(code = 500, message = "Error saving Player")
-////})
-////    @PostMapping(value = "/player/save",
-////            consumes = {"application/json"},
-////            produces = {"application/json"})
-////    public ResponseEntity<?> addNewPlayer(@Valid
-////                                            @RequestBody
-////                                                    Player newplayer) throws URISyntaxException
-////    {
-////        newplayer = playerService.save(newplayer);
-////
-////        // set the location header for the newly created resource
-////        HttpHeaders responseHeaders = new HttpHeaders();
-////        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newplayer.findPlayersByID("{id}")).toUri();
-////        responseHeaders.setLocation(newCustomerURI);
-////
-////        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
-////    }
+
 }
